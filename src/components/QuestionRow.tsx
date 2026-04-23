2f167d0d834b6ff31b959da98a626a3c85edfc4b
Dan Wolff <dan.d.wolff@gmail.com>
2026-04-24 00:03:16 +0200

    Layout: subgrid aligned columns, equal-width options, wider page, colorblind validity bars

--- src/components/QuestionRow.tsx
+++ src/components/QuestionRow.tsx
@@ -14,26 +14,27 @@ const LONG_THRESHOLD = 12;
 
 export function QuestionRow({ index, question, marks, validity, onOptionClick }: Props) {
   const isLong = question.options.some((o) => o.label.length > LONG_THRESHOLD);
+  const hasCorrect = marks.indexOf("correct") >= 0;
 
   return (
     <div class="question-row">
       <div class={`validity-bar ${validity}`} />
-      <div class="question-body">
-        <div class="question-header">
-          <span class="question-num">{index + 1}.</span>
-          <span class="question-text">{question.text}</span>
-        </div>
-        <div class={`question-options ${isLong ? "options-vertical" : ""}`}>
-          {question.options.map((opt, oi) => (
-            <OptionButton
-              key={opt.label}
-              index={oi}
-              label={opt.label}
-              mark={marks[oi]}
-              onClick={() => onOptionClick(oi)}
-            />
-          ))}
-        </div>
+      <div class="question-header">
+        <span class="question-num">{index + 1}.</span>
+        <span class="question-text">{question.text}</span>
+      </div>
+      <div class={`question-options ${isLong ? "options-vertical" : ""}`}>
+        {question.options.map((opt, oi) => (
+          <OptionButton
+            key={opt.label}
+            index={oi}
+            label={opt.label}
+            mark={marks[oi]}
+            implied={hasCorrect && marks[oi] === "unmarked"}
+            disabled={hasCorrect && marks[oi] !== "correct"}
+            onClick={() => onOptionClick(oi)}
+          />
+        ))}
       </div>
     </div>
   );
